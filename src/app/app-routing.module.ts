1d0c0bf469bb8bcf92ddde8ea4b11af2c9c6baca
Beimar  Escudero <beimarescudero@gmail.com>
2023-12-02 20:37:15 -0400

    proceso

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home.component';
+import { RegisterComponent } from './page/register/register.component';
+import { LoginComponent } from './page/login/login.component';
+import { LabsComponent } from './page/labs/labs.component';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 const routes: Routes = [
-  {path: '', component:HomeComponent}
+  { path: "", component:HomeComponent },
+  { path: "register", component:RegisterComponent },
+  { path: "login", component:LoginComponent},
+  { path: "labs", component:LabsComponent, ...canActivate(()=>redirectUnauthorizedTo(["/"]))}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,38 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './page/home/home.component';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+
+
+
+import { LoginComponent } from './page/login/login.component';
+import { LabsComponent } from './page/labs/labs.component';
+import { RegisterComponent } from './page/register/register.component';
+import { HeaderComponent } from './commons/header/header.component';
+const config = {
+  apiKey: "AIzaSyC1e4AK_VqnORu9rhZmw6m5yr8n6Tvjz6Y",
+  authDomain: "apis-5bf15.firebaseapp.com",
+  databaseURL: "https://apis-5bf15-default-rtdb.firebaseio.com",
+  projectId: "apis-5bf15",
+  storageBucket: "apis-5bf15.appspot.com",
+  messagingSenderId: "573756268196",
+  appId: "1:573756268196:web:91ce0b8c439ef17cdad031"
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    LabsComponent,
+    RegisterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    provideFirebaseApp(() => initializeApp(config)),
+    provideAuth(() => getAuth()), 
     AppRoutingModule,
     HttpClientModule,
     FormsModule
